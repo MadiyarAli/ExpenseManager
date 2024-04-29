340c332df0eab99395c0afc5cdc01717167939a8
Madiyar <Shadvence@gmail.com>
2024-04-29 22:18:59 +0500

    Initial Commit

---
+++ ExpenseManager/ExpenseManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseManagerApp.swift
+//  ExpenseManager
+//
+//  Created by Madiyar Ali on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
